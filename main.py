f952e90a1330fd2d738c5077b6c83f3125e8b7e6
Dylan Stitt <dstitt527@gmail.com>
2024-10-02 14:33:58 -0400

    adding matrices

--- main.py
+++ main.py
@@ -6,15 +6,12 @@ from samples import *
 from testcode import *
 
 def matAdd(m1, m2):
-    m1Dimensions = (len(m1), len(m1[0]))
-    m2Dimensions = (len(m2), len(m2[0]))
-    solution = [[] for i in range(m1Dimensions[0])]
+    solution = [[] for i in range(len(m1))]
 
-    if m1Dimensions[0] == m2Dimensions[0] and m1Dimensions[1] == m2Dimensions[1]:
-        for i in range(m1Dimensions[0]):
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+        for i in range(len(m1)):
             for j, k in enumerate(m1[i]):
                 solution[i].append(k + m2[i][j])
-
     return solution
 
 def matMul(m1, m2):
